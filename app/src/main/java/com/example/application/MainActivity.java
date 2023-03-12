6771546273dcd031ef14cedd46847ab05b0315f3
shacharbi23-meet <109904744+shacharbi23-meet@users.noreply.github.com>
2023-03-12 14:28:19 +0200

    Added intent to main activity to move to signup

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,10 +32,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v == SignIn){
-            Toast.makeText(this, "Sign up is under construction, please come back later!", Toast.LENGTH_LONG).show();
+        if (v == SignUp){
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
         }
-        else if (v == SignUp){
+        else if (v == SignIn){
             SignIn.setText(SignIn.getText().toString());
         }
     }
