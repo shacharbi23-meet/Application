458a3e820ba0583b67f232c8a0b1d0186a5ee04c
Shachar Biton <dawn.shachar1@gmail.com>
2023-03-12 21:13:12 +0200

    Create UserArrayAdapter.java

---
+++ app/src/main/java/com/example/application/UserArrayAdapter.java
@@ -0,0 +1,40 @@
+package com.example.application;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class UserArrayAdapter extends ArrayAdapter<User> {
+    private Context context;
+    private int resource;
+
+
+    public UserArrayAdapter(@NonNull Context context, int resource, ArrayList<User> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null){
+            convertView = LayoutInflater.from(this.context).inflate(this.resource, parent, false);
+        }
+        // Code goes here
+
+        return convertView;
+
+    }
+}
+
+
+
